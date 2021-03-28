bbef8750da160cd64f77978fd9545e77aa4b2637
Reko85 <65623351+Reko85@users.noreply.github.com>
2021-03-28 17:51:00 +0200

    Initial commit

---
+++ Problem2webDriver/Program.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+
+namespace Problem2webDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "http://www.google.com";
+            driver.Manage().Window.Maximize();
+
+            ICollection<IWebElement> links = driver.FindElements(By.TagName("a"));
+
+            Console.WriteLine("Link found: ");
+            foreach (var link in links)
+                Console.WriteLine(link.Text);
+        }
+    }
+}
